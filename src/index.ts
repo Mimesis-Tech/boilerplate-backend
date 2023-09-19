449e6c1a03db3d9aebfad3cbc3a7a41c4ff9d7ce
Robson-Carvalho <robsoncarvalho.tech@gmail.com>
2023-09-18 21:40:23 -0300

    feat: adiciona servidor typescript

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Boilerplate Backend Mímesis Tech");
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+const port = process.env.port || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Oee");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
